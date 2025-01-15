90b75cc47766e1b9bb02fa42920c7250ec68cb87
Yuzihang9 <yuzihang789@gmail.com>
2025-01-15 15:42:02 +0800

    steam规则更改

--- script.js
+++ script.js
@@ -21,6 +21,12 @@ function main(config) {
             proxies: ["HK","SG", "JP","KR", "US","TW","EU","AU"],
         },
         {
+            icon: "https://mirror.ghproxy.com/https://raw.githubusercontent.com/Orz-3/mini/master/Color/steam.png",
+            name: "Steam",  
+            type: "select",
+            proxies: ["DIRECT","HK","SG", "JP", "US","TW","EU","AU","KR"],
+        },
+        {
             icon: "https://mirror.ghproxy.com/https://raw.githubusercontent.com/Orz-3/mini/master/Color/github.png",
             name: "Github",  
             type: "select",
@@ -316,7 +322,7 @@ function main(config) {
         "RULE-SET,bard,AIGC",  
         "RULE-SET,openai,AIGC",
         "RULE-SET,claude,AIGC",
-        "RULE-SET,steam,DIRECT",
+        "RULE-SET,steam,Steam",
         "RULE-SET,steamcn,DIRECT",
         
         "RULE-SET,AppleNoChinaCDNRules,PROXY",
